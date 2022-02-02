5fe56252e9088501cafc38164f71b056a1758e7c
Wahyu Koco <74239131+Kakaranara@users.noreply.github.com>
2022-02-02 16:49:22 +0700

    styling navbar

---
+++ src/Navigation.js
@@ -0,0 +1,14 @@
+const Navigation = () => {
+    return (
+        <nav className="navbar">
+            <h1>My <Profile></Profile></h1>
+            <div className="links">
+                <a href="#">About Me</a>
+                <a href="#">Portofolio</a>
+                <a href="#">Sesuatu</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation;
